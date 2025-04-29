d67bdad741697d722e3ba96cd13f75ef1733f0e6
katiehellmann <kmh9516@g.rit.edu>
2025-04-29 16:10:43 -0400

    redis reconfigure

--- server/app.js
+++ server/app.js
@@ -1,75 +1,72 @@
-require('dotenv').config();
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const compression = require("compression");
+const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const expressHandlebars = require("express-handlebars");
+const helmet = require("helmet");
+const session = require("express-session");
+const RedisStore = require("connect-redis").default;
+const redis = require("redis");
 
-const path = require('path');
-const express = require('express');
-const compression = require('compression');
-const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const expressHandlebars = require('express-handlebars');
-const helmet = require('helmet');
-const session = require('express-session');
-const RedisStore = require('connect-redis').default;
-const redis = require('redis');
-
-const router = require('./router.js');
+const router = require("./router.js");
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/MessageMaker';
-mongoose.connect(dbURI).catch((err) => {
+const dbURL = process.env.MONGODB_URI || "mongodb://127.0.0.1/MessageMaker";
+mongoose.connect(dbURL).catch((err) => {
   if (err) {
-    console.log('couldnt connect');
+    console.log("Could not connect to database");
     throw err;
   }
 });
 
-// import { createClient } from 'redis';
-
 const redisClient = redis.createClient({
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: 'redis-19492.c114.us-east-1-4.ec2.redns.redis-cloud.com',
-    port: 19492,
-  },
+  url: process.env.REDISCLOUD_URL,
 });
 
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
 redisClient.connect().then(() => {
   const app = express();
 
+  app.set("trust proxy", 1);
+
   app.use(helmet());
-  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+  app.use("/assets", express.static(path.resolve(`${__dirname}/../hosted`)));
   app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
   app.use(compression());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-  app.use(session({
-    key: 'sessionid',
-    store: new RedisStore({
-      client: redisClient,
-    }),
-    secret: 'Message-Arigato',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
-      maxAge: 1000 * 60 * 60 * 24,
-    },
-  }));
+  app.use(
+    session({
+      key: "sessionId",
+      store: new RedisStore({ client: redisClient }),
+      secret: "Message-Arigato",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 1000 * 60 * 60 * 24,
+      },
+    })
+  );
 
-  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-  app.set('view engine', 'handlebars');
-  app.set('views', `${__dirname}/../views`);
+  app.engine("handlebars", expressHandlebars.engine({ defaultLayout: "" }));
+  app.set("view engine", "handlebars");
+  app.set("views", `${__dirname}/../views`);
 
   router(app);
 
   app.listen(port, (err) => {
-    if (err) { throw err; }
+    if (err) {
+      throw err;
+    }
     console.log(`Listening on port ${port}`);
   });
 });
